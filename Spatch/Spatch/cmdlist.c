790c1cec019f4608b1968ce2c5360c101e620ca7
Frederick Massin <massin_f@epitech.eu>
2015-02-15 16:38:41 +0100

    correction

--- Spatch/Spatch/cmdlist.c
+++ Spatch/Spatch/cmdlist.c
@@ -1,27 +1,30 @@
 #include <libssh/libssh.h>
-#include "usrfile.h"
+#include "usrFile.h"
 
 void cmd_createuser(char **cmd, ssh_channel channel)
 {
         if (cmd[0] != NULL && cmd[1] != NULL && cmd[2] != NULL)
         {
-            usrData *newuser = malloc(sizeof(usrData));
+            usrData *newuser = newUsrData();
             newuser->name = malloc(strlen(cmd[1]);
             newuser->pwd = malloc(strlen(cmd[2]);
 
 
             strcpy(newuser->name, cmd[1]);
             strcpy(newuser->pwd, cmd[2]);
-            if (strncmp(cmd[3], "USER"))
+            if (strncmp(cmd[3], "USER", 4))
                 newuser->spaAccess = USER;
-            else if (strncmp(cmd[3], "ADMIN"))
+            else if (strncmp(cmd[3], "ADMIN", 5))
                 newuser->spaAccess = ADMIN;
             else
-                newuser->spaAccess = NONE;
+            {
+                FreeUsrData(newuser);
+                ssh_channel_write(channel, ("usage \"cmd user pwd\"", strlen(("usage \"cmd user pwd\""));
+            }
             saveusrdata(newuser);
         }
             else
         {
-            ssh_channel_write(chan, ("usage \"cmd user pwd\"", strlen(("usage \"cmd user pwd\""));
+            ssh_channel_write(channel, ("usage \"cmd user pwd\"", strlen(("usage \"cmd user pwd\""));
         }
 }
