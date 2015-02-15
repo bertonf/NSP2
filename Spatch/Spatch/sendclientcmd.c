a19045bc27455556a3e798045d0de72c2699a36f
Frederick Massin <massin_f@epitech.eu>
2015-02-15 14:40:38 +0100

    sendclientcmd

--- Spatch/Spatch/sendclientcmd.c
+++ Spatch/Spatch/sendclientcmd.c
@@ -1,6 +1,6 @@
 #include <libssh/libssh.h>
 
-int show_remote_processes(ssh_session session)
+ssh_channel open_client_channel(ssh_session session)
 {
     ssh_channel channel;
     int rc;
@@ -13,8 +13,61 @@ int show_remote_processes(ssh_session session)
     if (rc != SSH_OK)
     {
         ssh_channel_free(channel);
+        return NULL;
+    }
+}
+
+int close_client_channel(ssh_channel channel)
+{
+    ssh_channel_send_eof(channel);
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+    return SSH_OK;
+}
+
+int send_cmd_to_ssh(int fd, ssh_channel channel, char *buff)
+{
+    int rc;
+
+    rc = ssh_channel_request_exec(channel, buff);
+    if (rc != SSH_OK)
+    {
+        ssh_channel_close(channel);
+        ssh_channel_free(channel);
         return rc;
     }
+    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+    while (nbytes > 0)
+    {
+        if (write(fd, buffer, nbytes) != nbytes)
+        {
+            ssh_channel_close(channel);
+            ssh_channel_free(channel);
+            return SSH_ERROR;
+        }
+        nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+    }
+    if (nbytes < 0)
+    {
+        ssh_channel_close(channel);
+        ssh_channel_free(channel);
+        return SSH_ERROR;
+    }
+    return rc;
+}
+
+
+int show_remote_processes(ssh_session session)
+{
+    ssh_channel channel;
+    int rc;
+    char buffer[256];
+    unsigned int nbytes;
+
+    channel = open_client_channel(session);
+    if (channel == NULL)
+        return SSH_ERROR;
+
     while (1)
     {
         char buff[2048];
